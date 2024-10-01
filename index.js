4245f03a732498bc7205e83d5ed4a9ff99041a2e
roselyne mwaniki <roselynewairimu30@gmail.com>
2024-10-01 22:02:39 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+// Code your solutions in this file.
+
+function writeCards(names) {
+    return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+  }
+  
+  
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
